6978daf46ba4e76762fb01e68e285154a3dc41d9
zer0VAns <nsilvahollger@gmail.com>
2022-08-31 19:37:41 -0300

    main

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function getAdvice() {
     w3.getHttpObject("https://api.adviceslip.com/advice", function (data) {
         advice = data.slip.advice
         text.innerHTML = '"' + advice + '"'
+        console.log(advice)
     })
 }
 const random = Math.floor(Math.random() * 150);
@@ -12,3 +13,4 @@ function randomNumber() {
     text2.innerHTML = "ADVICE #" + random
 }
 console.log(random)
+
