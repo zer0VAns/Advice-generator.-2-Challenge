bc0ace350c36d6895845f29705ce981c65072a90
zer0VAns <nsilvahollger@gmail.com>
2022-09-01 02:38:01 -0300

    main

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const text = document.getElementById('texto');
 const text2 = document.getElementById('texto2')
-console.log(text)
+// const random = Math.floor(Math.random() * 150);
 function getAdvice() {
-    w3.getHttpObject("https://api.adviceslip.com/advice", function (data) {
-        advice = data.slip.advice
-        text.innerHTML = '"' + advice + '"'
-        console.log(advice)
-    })
+    fetch("https://api.adviceslip.com/advice", {cache: "no-cache"})
+    .then( response => response.json())
+    .then( data => {
+       const advice = data.slip.advice
+       text.innerHTML = '"' + advice + '"'
+    //    text2.innerHTML = "ADVICE #" + random
+    }) 
+    function showRandomNumber(){
+        text2.innerHTML = "ADVICE #" + rando(1, 150);
+    }
+    showRandomNumber()
 }
-const random = Math.floor(Math.random() * 150);
-function randomNumber() {
-    text2.innerHTML = "ADVICE #" + random
-}
-console.log(random)
 
